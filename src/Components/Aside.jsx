6094857d7d8af7437a7a6158825fd58e10e9a0c1
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-11-02 12:57:43 +0530

    impleme nted navlink instead of Links for better visuals

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -1,40 +1,82 @@
-import React from 'react'
-import { CiUser , CiSearch , CiVideoOn , CiBasketball } from "react-icons/ci";
+import React from "react";
+import { CiUser, CiSearch, CiVideoOn, CiBasketball } from "react-icons/ci";
 import { FaHome } from "react-icons/fa";
 import { PiTelevisionSimple } from "react-icons/pi";
 import { BiCategory } from "react-icons/bi";
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 const Aside = () => {
   return (
-    <div className='bg-black text-white h-screen fixed top-0 w-[10vw]' >
-      <h1 className='text-center text-2xl font-bold text-amber-500' >Brand</h1>
-      <nav className='h-[70vh] flex flex-col items-center justify-around mt-12' >
-        <Link to="/login">
-        <i className='text-2xl cursor-pointer ' ><CiUser/></i>
-        </Link>
-        <i className='text-2xl cursor-pointer ' ><CiSearch/></i>
-        <Link to="/">
-        <i className='text-2xl cursor-pointer ' ><FaHome/></i>
-        </Link>
+    <div className="bg-black h-screen fixed top-0 w-[10vw]">
+      <h1 className="text-center text-2xl font-bold text-amber-500">Brand</h1>
+      <nav className="h-[70vh] flex flex-col items-center justify-around mt-12">
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? "text-amber-500" : "text-white"}`
+          }
+          to="/login"
+        >
+          <i className={`text-2xl cursor-pointer`}>
+            <CiUser />
+          </i>
+        </NavLink>
 
-        <Link to="/television">
-        <i className='text-2xl cursor-pointer ' ><PiTelevisionSimple/></i>
-        </Link>
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? "text-amber-500" : "text-white"}`
+          }
+          to="/"
+        >
+          <i className="text-2xl cursor-pointer ">
+            <FaHome />
+          </i>
+        </NavLink>
 
-        <Link to="/radio">
-        <i className='text-2xl cursor-pointer ' ><CiVideoOn/></i>
-        </Link>
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? "text-amber-500" : "text-white"}`
+          }
+          to="/television"
+        >
+          <i className="text-2xl cursor-pointer">
+            <PiTelevisionSimple />
+          </i>
+        </NavLink>
 
-        <Link to="/sports">
-        <i className='text-2xl cursor-pointer ' ><CiBasketball/></i>
-        </Link>
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? "text-amber-500" : "text-white"}`
+          }
+          to="/radio"
+        >
+          <i className="text-2xl cursor-pointer">
+            <CiVideoOn />
+          </i>
+        </NavLink>
 
-        <Link to="/category">
-        <i className='text-2xl cursor-pointer ' ><BiCategory/></i>
-        </Link>
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? "text-amber-500" : "text-white"}`
+          }
+          to="/sports"
+        >
+          <i className="text-2xl cursor-pointer">
+            <CiBasketball />
+          </i>
+        </NavLink>
+
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? "text-amber-500" : "text-white"}`
+          }
+          to="/category"
+        >
+          <i className="text-2xl cursor-pointer">
+            <BiCategory />
+          </i>
+        </NavLink>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Aside
+export default Aside;
